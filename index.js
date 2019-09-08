011d2d807537da0e8019c0365398d33817a3748b
reshadbinharun <md.bin_harun@tufts.edu>
2019-09-08 12:27:32 -0400

    server setup with mlab db created

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
+
+const DRUG_COLLECTION = 'Drugs';
+const MOLECULAR_MECHANISMS_COLLECTION = 'Mechanisms';
+
+const mongo = require('mongodb').MongoClient
 
 const PORT = 8080;
+
+const dbConnString = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds219308.mlab.com:19308/${process.env.DB_NAME}`;
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,20 +27,29 @@ app.use((req, res, next) => {
 })
 
 let corsOptions = {
-    // TODO: enforce before deploying
-    // origin: ['http://localhost:3000','https://onefootin-beta.herokuapp.com/'],
     origin: CORS_ALLOW_REGEX,
   }
 app.use(cors(corsOptions));
 
-/*
-add routes here
-*/
-app.get('/test', (req, res) => {
-    console.log("ping from client...");
-    res.status(200).send({
-        message: 'What\'s up?'
-    })
+mongo.connect(dbConnString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    }, (err, client) => {
+        if (err) {
+            console.error(err)
+            return;
+        }
+        const db = client.db(process.env.DB_NAME);
+        app.get('/test', (req, res) => {
+            console.log("ping from client...");
+            res.status(200).send({
+                message: 'What\'s up?'
+            })
+        })
+        /*
+        add routes here
+        */
+
 })
 
 app.listen(app.get('port'), () => {
